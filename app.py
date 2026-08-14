fe0840b779054a3c6d56a81c68f9e0144c7e021d
Pedro Lima <105872928+Pedro101520@users.noreply.github.com>
2026-08-14 15:47:20 -0300

    Pagina de visao geral

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from paginas.visao_geral import visao
+import pandas
 
-st.write("Teste")
+st.set_page_config(layout="wide")
+
+if "page" not in st.session_state:
+    st.session_state.page = "inicio"
+
+with st.sidebar:
+    st.write("### Navegação entre Páginas")
+    st.divider()
+    st.button("Visão Geral", use_container_width=True)
+
+
+
+if st.session_state.page == "inicio":
+    visao()
---
+++ paginas/visao_geral.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+def visao():
+    df = pd.read_csv(r"DATA\HR_Analytics_Cleaned_Master.csv")
+
+    st.title("Visão Geral")
+
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        container = st.container(height=120, border=False)
+        with container:
+            st.caption("## Total de Funcionários: ")
+        st.write(f"# {df.shape[0]}")
+    with col2:
+        container = st.container(height=120, border=False)
+        with container:
+            st.caption("## Taxa de demissão: ")
+        df_demissao = df.query('Attrition == "Yes"')
+        st.write(f"# {round((df_demissao.shape[0] / df.shape[0]), 3) * 100}%")
+    with col3:
+        container = st.container(height=120, border=False)
+        with container:
+            st.caption("## Idade média dos funcionários: ")
+        st.write(f"# {round(df["Age"].mean(), 1)}")
+    with col4:
+        container = st.container(height=120, border=False)
+        with container:
+            st.caption("## Salário Médio: ")
+        st.write(f"# {round(df["MonthlyIncome"].mean(), 2)}")
+
+    st.divider()
+
+    st.caption("## Funcionários por departamento: ")
+    df_departamento = df["Department"].value_counts().reset_index()
+    df_departamento.columns = ["Department", "funcionarios"]
+
+    st.bar_chart(
+        df_departamento,
+        x="Department",
+        y="funcionarios",
+        horizontal=True,
+        height=300
+    )
+
+    st.divider()
+
+    st.caption("## Funcionários por cargo: ")
+    df_cargo = df["JobRole"].value_counts().sort_values(ascending=True).reset_index()
+    df_cargo.columns = ["JobRole", "funcionarios"]
+
+    st.bar_chart(
+        df_cargo,
+        x="JobRole",
+        y="funcionarios",
+        horizontal=True,
+        height=300
+    )
+
+
